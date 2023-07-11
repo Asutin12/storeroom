2026e8f25628819713c4667213b145b685a1436a
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-11 22:33:43 +0900

    [style]add header

--- components/templates/Header/index.vue
+++ components/templates/Header/index.vue
@@ -1,17 +1,45 @@
 <template>
-    <div>
-        <div>
-            <title>合同会社優飛</title>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+    <div class="header">
+        <div class="header__global-nav">
+            <div class="header__global-nav__title">合同会社優飛</div>
+            <ul class="header__global-nav__list">
+                <li class="header__global-nav__item">事業内容</li>
+                <li class="header__global-nav__item">会社概要</li>
+                <li class="header__global-nav__item">カタログ</li>
             </ul>
         </div>
-        <button>採用情報</button>
+        <button class="header__button">採用情報</button>
     </div>
 </template>
 <script>
 </script>
-<style>
+<style lang="scss" scoped>
+.header {
+    height: 64px;
+    background-color: gray;
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    padding-inline: 24px;
+    align-items: center;
+    &__global-nav {
+        width: 400px;
+        justify-content: space-between;
+        display: flex;
+        align-items: center;
+        &__list {
+            width: 240px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    }
+    &__button {
+        width: 80px;
+        height: 32px;
+        background: #fff;
+        color: gray;
+        border-radius: 20px;
+    }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-    </div>
+    <div>mainView</div>
 </template>
 
 <script>
