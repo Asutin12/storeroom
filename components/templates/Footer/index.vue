ea0bddbd6432e3f4abdc868184f080fa99d93f6a
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-11 22:19:16 +0900

    add footer style

--- components/templates/Footer/index.vue
+++ components/templates/Footer/index.vue
@@ -6,22 +6,37 @@
             <li class="footer__item">カタログ</li>
             <li class="footer__item">採用情報</li>
         </ul>
-        <div class="footer__copyright">Copyright &copy; yuuhi,inc.All Rights Reserved.</div>
+        <hr class="footer__horizontalline">
+        <div class="footer__copyright">
+            <p>Copyright &copy; yuuhi,inc.All Rights Reserved.</p>
+        </div>
     </div>
 </template>
 <script>
 </script>
 <style lang="scss" scoped>
 .footer {
+    color: #fff;
+    width: 100%;
+    height: 100px;
+    background-color: gray;
+    padding-inline: 24px;
     &__list {
         width: 328px;
+        height: 59px;
         display: flex;
         align-items: center;
         justify-content: space-between;
         list-style: none;
+        margin-left: 80px;
+    }
+    &__horizontalline {
+        color: #fff;
     }
     &__copyright {
         margin-inline: auto;
+        text-align: center;
+        margin-block: 8px;
     }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        aaaa
     </div>
 </template>
 
