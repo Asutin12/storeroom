788b9a61eb45ed7993074f594e833cae8e2692db
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-11 21:39:53 +0900

    footer作成

---
+++ components/templates/Footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <ul class="footer__list">
+            <li class="footer__item">事業内容</li>
+            <li class="footer__item">企業情報</li>
+            <li class="footer__item">カタログ</li>
+            <li class="footer__item">採用情報</li>
+        </ul>
+        <div class="footer__copyright">Copyright &copy; yuuhi,inc.All Rights Reserved.</div>
+    </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+</style>
