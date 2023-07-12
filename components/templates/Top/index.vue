ef6a69e589515364ddeeb0d430a6ba8acac8dca8
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-12 21:26:18 +0900

    [create] TopComponent

---
+++ components/templates/Top/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="top">
+        <CompanyOverView />
+    </div>
+</template>
+<script>
+import CompanyOverView from '@/components/organisms/CompanyOverView'
+
+export default {
+    components: {
+        CompanyOverView
+    }
+}
+</script>
+<style lang="scss" scoped>
+.top {
+    background-color: #f5f5f5;
+}
+</style>
