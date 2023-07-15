7a3faa77deecacab11651c60d62d841ee291d1a6
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-15 21:48:41 +0900

    [feat]add todo

--- components/organisms/CompanyOverView/index.vue
+++ components/organisms/CompanyOverView/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="">
         <img class="overview-img" src="../../../assets/image/overview/image.svg" alt="CompanyOverview">
+        <div>TODO:会社概要</div>
     </div>
 </template>
 <style lang="scss" scoped>
