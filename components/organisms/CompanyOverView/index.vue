0cb09d218535cb80c7d2ba1ebbf7e083b0883af2
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-16 21:45:10 +0900

    [feat] add overview

--- components/organisms/CompanyOverView/index.vue
+++ components/organisms/CompanyOverView/index.vue
@@ -1,11 +1,37 @@
 <template>
-    <div class="">
-        <img class="overviewTitle" src="../../../assets/images/overview/title.svg" alt="CompanyOverview">
+    <div class="overview">
+        <img class="overview__title" src="../../../assets/images/overview/title.svg" alt="CompanyOverview">
         <div>
-            <img src="../../../assets/images/overview/subtitle.svg" alt="subTitle">
+            <img class="overview__subtitle" src="../../../assets/images/overview/subtitle.svg" alt="subTitle">
+            <div class="overview__contents">
+                <ContentsList />
+            </div>
         </div>
     </div>
 </template>
-<style lang="scss" scoped>
+<script>
+import ContentsList from '@/components/molecules/Overview/contentsList'
 
+export default {
+    components: {
+        ContentsList
+    }
+}
+</script>
+<style lang="scss" scoped>
+.overview {
+    max-width: 1200px;
+    margin-inline: auto;
+    &__title {
+        margin-inline: auto;
+        margin-bottom: 80px;
+    }
+    &__subtitle {
+        padding-bottom: 80px;
+    }
+    &__contents {
+        float: right;
+        width: 720px;
+    }
+}
 </style>
