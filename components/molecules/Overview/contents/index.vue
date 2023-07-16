c5dcf5b1a00e19c46efd259bd44a864ab1363aac
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-16 21:44:38 +0900

    [feat] add props

--- components/molecules/Overview/contents/index.vue
+++ components/molecules/Overview/contents/index.vue
@@ -1,16 +1,28 @@
 <template>
     <div class="content">
-        <h3>社名</h3>
-        <p>株式会社 山田建設</p>
+        <h3 class="content__category">{{ content.category }}</h3>
+        <p class="content__item">{{ content.item }}</p>
     </div>
 </template>
 <script>
+export default {
+    props: {
+        content:{
+            type: Object
+        }
+    }
+}
 </script>
 <style lang="scss" scoped>
 .content {
     display: flex;
-    justify-content: space-between;
     align-items: center;
-    border-bottom: 1px solid #fff;
+    border-bottom: 1px solid #000000;
+    &__category {
+        width: 150px;
+    }
+    &__item {
+        margin-left: 64px;
+    }
 }
 </style>
