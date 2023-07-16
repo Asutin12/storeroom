d1a67949eecd729ecd6ff7dc6ba9834db7fc018b
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-16 21:44:51 +0900

    [feat] add list

---
+++ components/molecules/Overview/contentsList/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="list">
+        <Contents v-for="content of contents" :key="content.id" :content="content" />
+    </div>
+</template>
+<script>
+import Contents from '@/components/molecules/Overview/contents'
+
+export default {
+    components: {
+        Contents
+    },
+    data() {
+        return {
+            contents: [
+                {
+                    category: '社名',
+                    item: '株式会社 山田建設'
+                },
+                {
+                    category:'設立',
+                    item: '2000年12月12日'
+                },
+                {
+                    category:'設立',
+                    item: '2000年12月12日'
+                },
+                {
+                    category:'設立',
+                    item: '2000年12月12日'
+                },
+                {
+                    category:'設立',
+                    item: '2000年12月12日'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+</style>
