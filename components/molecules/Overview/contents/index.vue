5d35188b7d99dd70affbbaa39fd79877b24e2dff
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-16 20:17:08 +0900

    [feat] add contents

---
+++ components/molecules/Overview/contents/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="content">
+        <h3>社名</h3>
+        <p>株式会社 山田建設</p>
+    </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+.content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #fff;
+}
+</style>
