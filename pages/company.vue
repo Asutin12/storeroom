48e4d302a59209792fb229ea3502334478afb7e9
Asutin12 <asuto12271818@docomo.ne.jp>
2023-07-22 22:47:22 +0900

    /company作成

---
+++ pages/company.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="company">
+        <CompanyOverView />
+    </div>
+</template>
+<script>
+import CompanyOverView from '@/components/organisms/CompanyOverView'
+
+export default {
+    components: {
+        CompanyOverView
+    }
+}
+</script>
+<style lang="scss" scoped>
+.company {
+    padding-block: 64px;
+    background-color: #f5f5f5;
+}
+</style>
